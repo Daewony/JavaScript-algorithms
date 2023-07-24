d473b70235f320fd640e5a7d64a11d3b141bc581
Daewony <alexnori.com@gmail.com>
2023-07-24 23:20:05 +0900

    Lv. 1

--- 프로그래머스/Lv. 1/푸드 파이트 대회.js
+++ 프로그래머스/Lv. 1/푸드 파이트 대회.js
@@ -31,7 +31,6 @@ function solution(food) {
     for (let i = 1; i < food.length; i++) {
         res += String(i).repeat(Math.floor(food[i] / 2));
     }
-
     return res + '0' + [...res].reverse().join('');
 }
 
