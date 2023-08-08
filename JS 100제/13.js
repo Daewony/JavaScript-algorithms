243ec8714b36019b1e445233abf4a546e74733ce
Daewony <alexnori.com@gmail.com>
2023-08-08 23:57:57 +0900

    Day 3

--- JS 100제/13.js
+++ JS 100제/13.js
@@ -1,23 +1,29 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
 
-const 행성 = ["", "수성", "금성", "지구", "화성", "목성", "토성", "천왕성", "해왕성"];
+// const 행성 = ["", "수성", "금성", "지구", "화성", "목성", "토성", "천왕성", "해왕성"];
 
-// console.log('값을 입력하세요:');
-rl.question('', (inx) => {
-    console.log("입력: ", parseInt(idx));
-    console.log("출력: ", 행성[idx]);
-    rl.close();
-});
-// rl.on('close', () => {
-//     console.log(Number(data[0]) + Number(data[1]));
-//     reader.close();
-//     process.exit();
-//   });
+// // console.log('값을 입력하세요:');
+// rl.question('', (inx) => {
+//     console.log("입력: ", parseInt(idx));
+//     console.log("출력: ", 행성[idx]);
+//     rl.close();
+// });
+// // rl.on('close', () => {
+// //     console.log(Number(data[0]) + Number(data[1]));
+// //     reader.close();
+// //     process.exit();
+// //   });
 
 
 
-// console.log(행성[0]);
+// // console.log(행성[0]);
+
+const planets = ['수성', '금성', '지구', '화성', '목성', '토성', '천왕성', '해왕성'];
+
+const n = prompt('몇 번째 행성인가요?');
+
+console.log(planets[n - 1]);
