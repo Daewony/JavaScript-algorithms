a3f5021cb99ecfb1541b096f49f8f71b8202ffef
Daewony <alexnori.com@gmail.com>
2023-09-05 23:59:57 +0900

    42

---
+++ JS 100제/42.js
@@ -0,0 +1,15 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    outpit: process.stdout
+});
+
+rl.question('a 입력: ', a =>{
+    
+    const Days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    
+    rl.question('b 입력: ',b=>{
+
+    })
+    rl.close();
+})
