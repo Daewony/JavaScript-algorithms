a22df82c8792cfcb6625681fd0edcd409a8b0d73
Daewony <alexnori.com@gmail.com>
2023-09-18 00:02:49 +0900

    46

--- JS 100제/46.js
+++ JS 100제/46.js
@@ -16,10 +16,25 @@
 // console.log(res);
 
 let arr = '';
+let sum = 0;
 for(let i=1;i<=20;i++){
     arr += i;
 }
 arr = arr.split('');
+for (const i of arr) {
+    sum+=parseInt(i);
+}
 
-console.log(arr);
+console.log(sum);
 
+
+let sum2 = 0;
+for (let i = 1; i <= 20; i++) {
+    let num = i;
+    while (num > 0) {
+        sum2 += num % 10; // 숫자의 마지막 자릿수를 더함
+        num = Math.floor(num / 10); // 마지막 자릿수를 제거
+    }
+}
+
+console.log(sum2);
