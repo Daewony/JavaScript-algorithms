59eaf00cbd6bcc731bfbfdf4b2cadb770c18b55a
Daewony <alexnori.com@gmail.com>
2023-09-23 00:00:07 +0900

    52

---
+++ JS 100제/52.js
@@ -0,0 +1,32 @@
+function quickSort(arr){
+    if (arr.length <= 1){
+      return arr;
+    }
+  
+    const pivot = arr[0];
+    const left = [];
+    const right = [];
+  
+    for (let i=1; i<arr.length; i++){
+      if(arr[i] < pivot){
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    }
+    return quickSort(left).concat(pivot, quickSort(right));
+  }
+
+  const readline = require('readline');
+  const rl = require('readline').createInterface({
+    input:process.stdin,
+    output:process.stdout
+  });
+  rl.question('배열을 입력하세요', n.split(' ').map(n=>parseInt(n,10))=>{
+    rl.close();
+  } );
+
+  
+//   const array = prompt('배열을 입력하세요').split(' ').map(n => parseInt(n, 10));
+  
+  console.log(quickSort(array));
