5f8eb29900f5eab8933a7dc5ffcc655e73ea0642
Daewony <alexnori.com@gmail.com>
2024-01-08 14:14:16 +0900

    같은숫자는싫어 복습

--- 프로그래머스/Lv. 1/같은숫자는싫어.js
+++ 프로그래머스/Lv. 1/같은숫자는싫어.js
@@ -15,6 +15,13 @@ function solution(arr) {
 function solution(arr) {
   return arr.filter((val, index) => val != arr[index + 1]);
 }
+// filter함수는 배열을 한번 스캔해서 다음에 나오는 것을 알기때문에 비교가능
+function solution(arr) {
+  return arr.filter((val, index) => {
+    console.log(`val: ${val}, index: ${index}`);
+    return val != arr[index + 1];
+  });
+}
 
 function solution(arr) {
   let answer = [];
@@ -28,6 +35,22 @@ function solution(arr) {
     if (arr[i] !== answer[cur2]) {
       answer.push(arr[i]);
       cur2++;
+    }
+  }
+
+  return answer;
+}
+
+// 커서 없이 배열의 길이를 활용하여 비교하기
+function solution(arr) {
+  var answer = [arr[0]];
+
+  for (let i = 1; i < arr.length; i++) {
+    console.log(
+      `answer: ${answer} arr[i]: ${arr[i]} ${answer[answer.length - 1]}`
+    );
+    if (answer[answer.length - 1] !== arr[i]) {
+      answer.push(arr[i]);
     }
   }
 
