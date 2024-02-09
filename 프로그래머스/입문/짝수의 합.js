2692eedf037d9cef85b91a0d187a9ef238c28a95
Daewony <alexnori.com@gmail.com>
2024-02-10 00:23:30 +0900

    20240210

---
+++ 프로그래머스/입문/짝수의 합.js
@@ -0,0 +1,15 @@
+// function solution(n) {
+//   let sum = 0;
+//   for (let i = 2; i <= n; i++) {
+//     if(i%2===0) sum += i;
+//   }
+//   return sum;
+// }
+
+function solution(n) {
+  let sum = 0;
+  for (let i = 2; i <= n; i += 2) {
+    sum += i;
+  }
+  return sum;
+}
