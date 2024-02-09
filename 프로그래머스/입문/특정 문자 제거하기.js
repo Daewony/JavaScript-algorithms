6b689c34194e9073e72641b3566ed65d5750f2d3
Daewony <alexnori.com@gmail.com>
2024-02-10 00:13:08 +0900

    20240210

--- 프로그래머스/입문/특정 문자 제거하기.js
+++ 프로그래머스/입문/특정 문자 제거하기.js
@@ -1,9 +1,17 @@
+// function solution(my_string, letter) {
+//     let answer = [...my_string];
+//     for(let i=0;i<answer.length;i++){
+//         if(answer[i]===letter){
+//             answer[i]='';
+//         }
+//     }
+//     return answer.join('');
+// }
+
 function solution(my_string, letter) {
-  let answer = [...my_string];
-  for (let i = 0; i < answer.length; i++) {
-    if (answer[i] === letter) {
-      answer[i] = "";
-    }
+  let answer = "";
+  for (let i = 0; i < my_string.length; i++) {
+    if (my_string[i] !== letter) answer += my_string[i];
   }
-  return answer.join("");
+  return answer;
 }
