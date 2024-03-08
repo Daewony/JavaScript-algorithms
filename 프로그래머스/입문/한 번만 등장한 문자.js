d565e9dd2577f47bee0ae47081a58b3e4738706b
Daewony <alexnori.com@gmail.com>
2024-03-09 00:09:33 +0900

    20240309

--- 프로그래머스/입문/한 번만 등장한 문자.js
+++ 프로그래머스/입문/한 번만 등장한 문자.js
@@ -1,5 +1,11 @@
+// 정렬을 한 후에 같은 알파벳이 없는지 확인하고 출력하기
+
 function solution(s) {
   var answer = "";
-  asd
+
+  s = s.split("").sort().join("");
+  for (let i = 0; i < s.length; i++) {
+    if (s.split(s[i]).length === 2) answer += s[i];
+  }
   return answer;
 }
