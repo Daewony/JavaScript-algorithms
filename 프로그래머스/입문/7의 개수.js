f8db2291a5534f131ff87028b4bb0dbb9f768271
Daewony <alexnori.com@gmail.com>
2024-04-03 00:00:33 +0900

    20240403

--- 프로그래머스/입문/7의 개수.js
+++ 프로그래머스/입문/7의 개수.js
@@ -1,6 +1,8 @@
 function solution(array) {
-    var answer = 0;
-    let arr = array.join('').split('')
-    for()
-    return answer;
+  var answer = 0;
+  let arr = array.join("").split("");
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "7") answer += 1;
+  }
+  return answer;
 }
