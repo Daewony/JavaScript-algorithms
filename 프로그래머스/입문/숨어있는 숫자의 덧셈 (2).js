f9eb882e5e11b9658a1516998c2d6d6a65a384e3
Daewony <alexnori.com@gmail.com>
2024-04-06 00:19:13 +0900

    20240406

--- 프로그래머스/입문/숨어있는 숫자의 덧셈 (2).js
+++ 프로그래머스/입문/숨어있는 숫자의 덧셈 (2).js
@@ -1,12 +1,22 @@
+//
+
 function solution(my_string) {
   var answer = 0;
   let arr = my_string.split("");
   let s = [];
-  for (let i = 0; arr.length; i++) {
-    if (arr[i] >= "0" && arr[i] < "9") {
-      s.push(arr[i]);
+  let a = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= "0" && arr[i] <= "9") {
+      a += arr[i];
+      if (!(arr[i + 1] >= "0" && arr[i + 1] <= "9")) {
+        s.push(a);
+        a = "";
+      }
     }
   }
+  answer = s.map(Number).reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
 
   return answer;
 }
