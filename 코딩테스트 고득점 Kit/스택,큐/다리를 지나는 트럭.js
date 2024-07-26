6e3db1793ac5b68f76e557eb9b3a646307c2c18e
Daewony <alexnori.com@gmail.com>
2024-07-26 23:47:51 +0900

    20240726

--- 코딩테스트 고득점 Kit/스택,큐/다리를 지나는 트럭.js
+++ 코딩테스트 고득점 Kit/스택,큐/다리를 지나는 트럭.js
@@ -3,34 +3,26 @@
 // 혹시 트럭이 2대 들어가는 다리니까 2초 걸린다는 의미인건가? -> 맞는거같음
 // 1초에 출발함
 
-function arraySum(arr[]) {
-    
-    let sum = 0;
-    for(let i of arr) {
-        sum+=i;
-    }
-    
-    return i;
+function arraySum(arr) {
+  let sum = 0;
+  for (let i of arr) {
+    sum += i;
+  }
+
+  return i;
 }
 
-
 function solution(bridge_length, weight, truck_weights) {
-    let cnt = 0;
-    let fs = [];
-    let ing = [];
-    
-    console.log(Math.max(truck_weights))
-    // 다리를 지난 트럭이 다 건너면 반복문 종료
-    while(fs.length !== truck_weights.length){
-        
-        // 다리를 건너는 트럭 추가
-        
-        break;
-        
-        // 다리를 지난 트럭 조건
-        
-        
-    }
-    
-    return cnt;
+  let cnt = 0;
+  let fs = [];
+  let ing = [];
+
+  console.log(Math.max(truck_weights));
+  // 다리를 지난 트럭이 다 건너면 반복문 종료
+  while (fs.length !== truck_weights.length) {
+    // 다리를 건너는 트럭 추가
+    // 다리를 지난 트럭 조건
+  }
+
+  return cnt;
 }
