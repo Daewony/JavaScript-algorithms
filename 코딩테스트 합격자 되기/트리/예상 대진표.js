09eb0b9bbea27d68f3787dfa4fecf3119fe1969c
Daewony <alexnori.com@gmail.com>
2024-08-06 23:45:57 +0900

    20240806

--- 코딩테스트 합격자 되기/트리/예상 대진표.js
+++ 코딩테스트 합격자 되기/트리/예상 대진표.js
@@ -1,5 +1,14 @@
+// 그나마 생각이 떠오르는 것은 트리 구조
+// 절반
+
 function solution(n, a, b) {
   var answer = 0;
+
+  while (a !== b) {
+    a = Math.floor((a + 1) / 2);
+    b = Math.floor((b + 1) / 2);
+    answer++;
+  }
 
   return answer;
 }
