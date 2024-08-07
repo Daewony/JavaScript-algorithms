d7daefcfe788bbc672907f2145d6f3d9fa31ee9b
Daewony <alexnori.com@gmail.com>
2024-08-07 16:13:12 +0900

    20240807

--- 코딩테스트 고득점 Kit/스택,큐/다리를 지나는 트럭.js
+++ 코딩테스트 고득점 Kit/스택,큐/다리를 지나는 트럭.js
@@ -1,28 +1,28 @@
-// 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지
-// 왜 7kg 트럭이 1~2초 걸리는지 이해가 안됨
-// 혹시 트럭이 2대 들어가는 다리니까 2초 걸린다는 의미인건가? -> 맞는거같음
-// 1초에 출발함
-
-function arraySum(arr) {
-  let sum = 0;
-  for (let i of arr) {
-    sum += i;
-  }
-
-  return i;
-}
-
 function solution(bridge_length, weight, truck_weights) {
-  let cnt = 0;
-  let fs = [];
-  let ing = [];
+  let time = 0;
+  let bridge = [];
+  let bridgeWeight = 0;
 
-  console.log(Math.max(truck_weights));
-  // 다리를 지난 트럭이 다 건너면 반복문 종료
-  while (fs.length !== truck_weights.length) {
-    // 다리를 건너는 트럭 추가
-    // 다리를 지난 트럭 조건
+  while (truck_weights.length > 0 || bridge.length > 0) {
+    // 시간 경과
+    time++;
+
+    // 다리 위의 트럭을 하나씩 전진시키고, 다리에서 나갈 트럭을 제거
+    if (bridge.length > 0 && bridge[0].endTime === time) {
+      bridgeWeight -= bridge[0].weight;
+      bridge.shift();
+    }
+
+    // 새 트럭이 다리에 진입할 수 있는지 확인
+    if (truck_weights.length > 0 && bridgeWeight + truck_weights[0] <= weight) {
+      const currentTruckWeight = truck_weights.shift();
+      bridge.push({
+        weight: currentTruckWeight,
+        endTime: time + bridge_length,
+      });
+      bridgeWeight += currentTruckWeight;
+    }
   }
 
-  return cnt;
+  return time;
 }
