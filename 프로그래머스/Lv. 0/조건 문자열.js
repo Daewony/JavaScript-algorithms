592716406d645bb7311847e6d7f2779478218ae0
Daewony <alexnori.com@gmail.com>
2024-08-30 23:27:48 +0900

    20240830

---
+++ 프로그래머스/Lv. 0/조건 문자열.js
@@ -0,0 +1,18 @@
+function solution(ineq, eq, n, m) {
+  var answer = 0;
+  if (ineq === ">") {
+    if (eq === "=") {
+      if (n >= m) answer = 1;
+    } else {
+      if (n > m) answer = 1;
+    }
+  } else if (ineq === "<") {
+    if (eq === "=") {
+      if (n <= m) answer = 1;
+    } else {
+      if (n < m) answer = 1;
+    }
+  }
+
+  return answer;
+}
