fb270a767f9bf827fb8603d2a3ca0e3f0d07d930
Daewony <alexnori.com@gmail.com>
2024-08-30 23:36:08 +0900

    20240830

--- 프로그래머스/Lv. 0/조건 문자열.js
+++ 프로그래머스/Lv. 0/조건 문자열.js
@@ -1,3 +1,4 @@
+// 나의 풀이
 function solution(ineq, eq, n, m) {
   var answer = 0;
   if (ineq === ">") {
@@ -15,4 +16,37 @@ function solution(ineq, eq, n, m) {
   }
 
   return answer;
+}
+
+// GPT 방식
+
+function solution(ineq, eq, n, m) {
+  const condition = ineq + eq;
+
+  switch (condition) {
+    case ">=":
+      return n >= m ? 1 : 0;
+    case "<=":
+      return n <= m ? 1 : 0;
+    case ">":
+      return n > m ? 1 : 0;
+    case "<":
+      return n < m ? 1 : 0;
+    default:
+      return 0;
+  }
+}
+
+// 다른사람 풀이
+
+const operations = {
+  ">=": (n, m) => n >= m,
+  "<=": (n, m) => n <= m,
+  ">!": (n, m) => n > m,
+  "<!": (n, m) => n < m,
+};
+
+function solution(ineq, eq, n, m) {
+  const op = operations[ineq + eq];
+  return Number(op(n, m));
 }
