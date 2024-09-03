5ec874a1dfb79e2865a9430539550cd3d3ddc6fa
Daewony <alexnori.com@gmail.com>
2024-09-03 15:31:21 +0900

    20240903

--- 프로그래머스/Lv. 0/n개 간격의 원소들.js
+++ 프로그래머스/Lv. 0/n개 간격의 원소들.js
@@ -14,3 +14,11 @@ function solution(num_list, n) {
   });
   return answer;
 }
+
+// filter & map 조합 풀이
+
+function solution(num_list, n) {
+  return num_list
+    .map((num, i) => (i % n === 0 ? num : null))
+    .filter((num) => num !== null);
+}
