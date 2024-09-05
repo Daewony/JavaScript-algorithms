90b5478bbe62dedb5581b167b1d96a61b6a33118
Daewony <alexnori.com@gmail.com>
2024-09-05 13:18:52 +0900

    20240905

--- 프로그래머스/Lv. 0/부분 문자열 이어 붙여 문자열 만들기.js
+++ 프로그래머스/Lv. 0/부분 문자열 이어 붙여 문자열 만들기.js
@@ -7,3 +7,12 @@ function solution(my_strings, parts) {
 
   return answer;
 }
+
+// 다른 사람의 풀이
+function solution(my_strings, parts) {
+  return parts
+    .map(([s, e], i) => {
+      return my_strings[i].slice(s, e + 1);
+    })
+    .join("");
+}
