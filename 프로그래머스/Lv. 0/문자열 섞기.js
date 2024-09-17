ad11c00d1c776b04798bb7e5246d1f25e5f7d205
Daewony <alexnori.com@gmail.com>
2024-09-17 23:29:55 +0900

    20240917

---
+++ 프로그래머스/Lv. 0/문자열 섞기.js
@@ -0,0 +1,15 @@
+function solution(str1, str2) {
+  let i = 1;
+  str1 = [...str1];
+  for (const s of str2) {
+    str1.splice(i, 0, s);
+    i += 2;
+  }
+  return str1.join('');
+}
+
+// 다른 사람 풀이
+
+function solution(str1, str2) {
+  return [...str1].map((x, idx) => x + str2[idx]).join('');
+}
