f841edfadebd832175fb1e9ed40bd7bc4a375045
Daewony <alexnori.com@gmail.com>
2024-09-19 16:49:48 +0900

    20240919

--- 프로그래머스/Lv. 0/날짜 비교하기.js
+++ 프로그래머스/Lv. 0/날짜 비교하기.js
@@ -14,3 +14,17 @@ function solution(date1, date2) {
 
   return 0;
 }
+
+// 다른 사람 풀이
+function solution(date1, date2) {
+  const d1 = new Date(date1[0], date1[1] - 1, date1[2]);
+  const d2 = new Date(date2[0], date2[1] - 1, date2[2]);
+
+  return d1 < d2 ? 1 : 0;
+}
+
+const solution = (date1, date2) => (new Date(date1) < new Date(date2) ? 1 : 0);
+
+function solution(date1, date2) {
+  return date1.join('') - date2.join('') < 0 ? 1 : 0;
+}
