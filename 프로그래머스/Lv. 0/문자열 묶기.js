257d88a7e6ba9876ab789949bd0c9766c28ccdc6
Daewony <alexnori.com@gmail.com>
2024-09-23 23:07:51 +0900

    20240923

--- 프로그래머스/Lv. 0/문자열 묶기.js
+++ 프로그래머스/Lv. 0/문자열 묶기.js
@@ -1,5 +1,4 @@
 function solution(strArr) {
-  var answer = 0;
   let arr = Array(30).fill(0);
   for (const s of strArr) {
     let len = s.length;
@@ -9,4 +8,25 @@ function solution(strArr) {
   arr = arr.map((v) => (v === undefined ? 0 : v));
 
   return Math.max(...arr);
+}
+
+// 다른 사람의 풀이
+function solution(strArr) {
+  let arr = Array(30).fill(0); // 길이 30까지의 배열을 0으로 채우기
+
+  // 문자열 길이에 따라 카운트
+  for (const s of strArr) {
+    arr[s.length]++; // 문자열 길이(len)의 인덱스를 증가
+  }
+
+  // 배열에서 가장 큰 값을 반환 (가장 많은 그룹의 크기)
+  return Math.max(...arr);
+}
+
+function solution(strArr) {
+  const counter = new Map();
+  for (const str of strArr) {
+    counter.set(str.length, (counter.get(str.length) || 0) + 1);
+  }
+  return Math.max(...counter.values());
 }
