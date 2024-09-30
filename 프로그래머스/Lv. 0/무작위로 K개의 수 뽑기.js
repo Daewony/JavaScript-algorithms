3a9ba9fe0c2268d24ba74ce4452a8340bd2d9a70
Daewony <alexnori.com@gmail.com>
2024-09-30 21:42:41 +0900

    20240930

--- 프로그래머스/Lv. 0/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/Lv. 0/무작위로 K개의 수 뽑기.js
@@ -35,3 +35,10 @@ function solution(arr, k) {
 
   return answer;
 }
+
+function solution(arr, k) {
+  const set = new Set(arr);
+  return set.size < k
+    ? [...set, ...Array(k - set.size).fill(-1)]
+    : [...set].slice(0, k);
+}
