eea11262fdaf36009aa4a1bb95d6c4131a104cc4
Daewony <alexnori.com@gmail.com>
2024-09-30 21:38:41 +0900

    20240930

---
+++ 프로그래머스/Lv. 0/무작위로 K개의 수 뽑기.js
@@ -0,0 +1,37 @@
+// 길이 k 배열
+// 서로 다른 수를 배열에 push하기
+// 배열의 길이가 k일때까지 나머지 값을 -1 로 push하기
+
+function solution(arr, k) {
+  const answer = [];
+
+  for (const a of arr) {
+    if (!answer.includes(a) && answer.length < k) answer.push(a);
+  }
+
+  while (answer.length < k) {
+    answer.push(-1);
+  }
+
+  return answer;
+}
+
+// 다른 사람 풀이
+function solution(arr, k) {
+  const answer = [];
+  const seen = new Set();
+
+  for (const a of arr) {
+    if (!seen.has(a)) {
+      seen.add(a);
+      answer.push(a);
+      if (answer.length === k) break;
+    }
+  }
+
+  while (answer.length < k) {
+    answer.push(-1);
+  }
+
+  return answer;
+}
