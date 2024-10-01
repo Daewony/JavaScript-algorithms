eb95711b85d20fab9b2ebdff324b8e0b35426dfc
Daewony <alexnori.com@gmail.com>
2024-10-01 17:15:29 +0900

    20241001

--- 프로그래머스/Lv. 0/등수 매기기.js
+++ 프로그래머스/Lv. 0/등수 매기기.js
@@ -15,3 +15,23 @@ function solution(score) {
 
   return answer;
 }
+
+// 다른 사람 풀이
+function solution(score) {
+  // 각 학생의 평균 점수를 계산한 배열을 만듦
+  const avg = score.map(([e, m]) => (e + m) / 2);
+
+  // 평균 점수를 기준으로 내림차순 정렬한 배열을 만듦
+  const sortedAvg = [...avg].sort((a, b) => b - a);
+
+  // 정렬된 배열의 순위에 따라 등수를 부여
+  return avg.map((v) => sortedAvg.indexOf(v) + 1);
+}
+
+function solution(score) {
+  return score.map((el) => {
+    return (
+      score.filter((v) => (v[0] + v[1]) / 2 > (el[0] + el[1]) / 2).length + 1
+    );
+  });
+}
