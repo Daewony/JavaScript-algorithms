107d5934f17ca15e6e903ace51ab81aaf0616694
Daewony <alexnori.com@gmail.com>
2024-10-18 18:26:33 +0900

    20241018

--- 프로그래머스/Lv. 0/문자열 뒤집기.js
+++ 프로그래머스/Lv. 0/문자열 뒤집기.js
@@ -5,3 +5,17 @@ function solution(my_string, s, e) {
   a.splice(s, e - s + 1, rev);
   return a.join('');
 }
+
+// 다른 사람 풀이
+function solution(my_string, s, e) {
+  // 앞부분 + 뒤집을 부분 + 뒷부분을 이어붙임
+  return (
+    my_string.slice(0, s) + // s 이전 부분
+    my_string
+      .slice(s, e + 1)
+      .split('')
+      .reverse()
+      .join('') + // s부터 e까지 뒤집음
+    my_string.slice(e + 1) // e 이후 부분
+  );
+}
