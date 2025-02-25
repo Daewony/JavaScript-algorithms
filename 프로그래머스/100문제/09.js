8b6b2f91250a0912db75dd3c8ff7f30c47cd7771
강대원 <alexnori.com@gmail.com>
2025-02-26 00:00:04 +0900

    20250225

--- 프로그래머스/100문제/09.js
+++ 프로그래머스/100문제/09.js
@@ -24,3 +24,33 @@ function solution(num) {
 console.log(solution(10)); // 반환값 :  1010
 console.log(solution(27)); // 반환값 :  11011
 console.log(solution(12345)); // 반환값 : 11000000111001
+
+// 
+
+function solution2(decimal) {
+    const stack = [];
+    while(decimal > 0) {
+        const remainder = decimal % 2;
+        stack.push(remainder);
+        decimal = Math.floor(decimal / 2);
+    }
+
+    let binary = "";
+    while(stack.length > 0) {
+        binary += stack.pop();
+    }
+
+    return binary;
+}
+
+console.log(solution2(10)); // 반환값 :  1010
+console.log(solution2(27)); // 반환값 :  11011
+console.log(solution2(12345)); // 반환값 : 11000000111001
+
+function solution3(num) {
+    return num.toString(2);
+}
+
+console.log(solution3(10)); // 반환값 :  1010
+console.log(solution3(27)); // 반환값 :  11011
+console.log(solution3(12345)); // 반환값 : 11000000111001
